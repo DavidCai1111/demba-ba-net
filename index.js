5559aca7f00581a4386a5f16b121e6a9564e9bca
David Cai <davidcai1993@yahoo.com>
2018-07-23 20:21:39 +0800

    feat: use commander

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 'use strict'
 require('@tensorflow/tfjs-node')
-const tf = require('@tensorflow/tfjs')
-const image = require('./lib/image')
-const { loadMobileNet } = require('./lib/model')
+const application = require('commander')
+const pkg = require('./package')
 
-;(async function () {
-  const mobileNet = await loadMobileNet()
-  const model = await tf.loadModel(`file://${__dirname}/demba-ba-net/model.json`)
-  const img = await image.loadImage('../images/demba ba/11. neymar.jpg')
+application.version(pkg.version)
 
-  const activation = mobileNet.predict(img)
+const MODEL_PATH = `file://${__dirname}/demba-ba-net/model.json`
 
-  console.log(activation)
+application
+  .command('infer')
+  .description('Check whether a picture contains Demba Ba')
+  .option('-i, --inputImagePath <path>', 'Path to the input image')
+  .option('-g, --gpu')
+  .action(function (opts) {
+    ;(async function () {
+      const { inputImagePath, gpu } = opts
 
-  const result = await model.predict(activation)
+      if (gpu) require('@tensorflow/tfjs-node-gpu')
+      else require('@tensorflow/tfjs-node')
 
-  console.log(result.dataSync())
-})(console.error)
+      const tf = require('@tensorflow/tfjs')
+      const image = require('./lib/image')
+      const { loadMobileNet } = require('./lib/model')
+
+      const mobileNet = await loadMobileNet()
+      const model = await tf.loadModel(MODEL_PATH)
+
+      const img = await image.loadImage(inputImagePath)
+
+      const activation = await mobileNet.predict(img)
+
+      const result = await model.predict(activation)
+
+      console.log({ possibility: `${Number(result.dataSync()) * 100}%` })
+    })(console.error)
+  })
+
+application.parse(process.argv)
